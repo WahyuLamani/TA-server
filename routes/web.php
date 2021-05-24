3f2bbb68dad2f68fa21389ab16a935e56871b207
unknown <wahyu@elektro.polimdo.ac.id>
2021-05-24 23:43:39 +0800

    activation forgot password and email verified

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/profile', [HomeController::class, 'profile'])->name('profile');
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
-
-Route::get('/profile/edit', [HomeController::class, 'edit'])->name('profile.edit');
-Route::patch('/profile/update/{user:id}', [HomeController::class, 'update'])->name('profile.update');
+Route::middleware('verified')->group(function () {
+    Route::get('/profile', [HomeController::class, 'profile'])->name('profile');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+    Route::get('/profile/edit', [HomeController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile/update/{user:id}', [HomeController::class, 'update'])->name('profile.update');
+});
