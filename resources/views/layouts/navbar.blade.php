e604cf3032ab0f3e9db9399c1c712b705dc23da2
unknown <wahyu@elektro.polimdo.ac.id>
2021-07-14 23:03:39 +0800

    fix bug post

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -40,7 +40,7 @@
                                     <ul>
                                        @foreach (session('agent') as $row)
                                             <li class="notification-unread">
-                                                <a href="post/agent/{{ $row->id }}">
+                                                <a href="/post/agent/{{ $row->id }}">
                                                     <img class="float-left mr-3 avatar-img" src="/storage/{{$row->owner->thumbnail}}" alt="">
                                                     <div class="notification-content">
                                                         <div class="notification-heading">{{ $row->owner->name }}</div>
