d580e1a63bcd88647dcb44d1509db6cb806c6764
unknown <wahyu@elektro.polimdo.ac.id>
2021-07-16 20:18:59 +0800

    fix bug post detail

--- app/Http/Controllers/Dashboard/AgentPostController.php
+++ app/Http/Controllers/Dashboard/AgentPostController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Client\Agent;
 use App\Models\Client\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 use function Ramsey\Uuid\v1;
 
@@ -12,6 +14,9 @@ class AgentPostController extends Controller
 {
     public function detail(Post $post)
     {
+        if ($post->owner->company_id !== Auth::user()->userable->id) {
+            abort(404);
+        }
         return view('reporting.agent-reporting', compact('post'));
     }
 }
