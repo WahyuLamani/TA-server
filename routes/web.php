bf4eccca71561c84ea50cad6fad95894ae2e4f6c
wahyulamani <wahyu@elektro.polimdo.ac.id>
2021-07-30 22:56:58 +0800

    add middleware at route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 
 use App\Http\Controllers\Auth\UpdateUserController;
+use App\Http\Controllers\Clients\ClientRegisterController;
 use App\Http\Controllers\Clients\GetOrderController;
 use App\Http\Controllers\Clients\HomeController as ClientsHomeController;
 use App\Http\Controllers\Dashboard\{AgentPostController, CompanyPostController, DistributorPostController, ProfileController, ShowOrderController};
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\{Auth, Route};
 
 // Auth::routes();
 // Auth::routes(['verify' => true]);
+Route::view('/agent-register', 'clients.register.agent-register')->name('agent.register');
+Route::post('/agent-register', [ClientRegisterController::class, 'agentRegister']);
+Route::patch('/agent-register', [ClientRegisterController::class, 'agentRegisterStepTwo'])->name('agent.form');
+// Route::view('/agent-form', 'clients.register.agent-register')->name('agent.form');
+Route::view('/distributor-register', 'clients.register.distributor-register')->name('distributor.register');
+Route::post('/distributor-register', [ClientRegisterController::class, 'distributorRegister']);
+
 Auth::routes();
 
 // Route::middleware('verified')->group(function () {
@@ -20,15 +28,16 @@ Route::middleware('auth')->group(function () {
 
     Route::get('/', [HomeController::class, 'handle'])->name('handle');
     Route::post('/', [CompanyController::class, 'create'])->name('company.create');
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('server');
 
-    Route::get('/clients', [ClientsHomeController::class, 'index'])->name('clients');
-    Route::post('/clients', [GetOrderController::class, 'order']);
-    Route::patch('/clients/{order:id}', [GetOrderController::class, 'acceptOrder']);
-    Route::post('/clients/{order:id}', [GetOrderController::class, 'distributed']);
-    Route::patch('/clients/accept/{order:id}', [GetOrderController::class, 'completeOrder']);
-    Route::delete('/clients/delete/{order:id}', [GetOrderController::class, 'delete']);
-
+    Route::group(['middleware' => 'client', 'prefix' => 'clients'], function () {
+        Route::get('/', [ClientsHomeController::class, 'index'])->name('clients');
+        Route::post('/', [GetOrderController::class, 'order']);
+        Route::patch('{order:id}', [GetOrderController::class, 'acceptOrder']);
+        Route::post('{order:id}', [GetOrderController::class, 'distributed']);
+        Route::patch('accept/{order:id}', [GetOrderController::class, 'completeOrder']);
+        Route::delete('delete/{order:id}', [GetOrderController::class, 'delete']);
+    });
 
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
     Route::post('/profile', [CompanyPostController::class, 'posting'])->name('profile.posting');
@@ -36,28 +45,30 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile/update/{user:id}', [UpdateUserController::class, 'update'])->name('profile.update');
 
-    Route::get('/agents', [AgentsController::class, 'index'])->name('agents');
-    Route::post('/agent/store', [AgentsController::class, 'store'])->name('agent.store');
-    Route::get('/agent/details/{agent:id}', [AgentsController::class, 'details']);
-    Route::delete('/agent/delete/{agent:id}', [AgentsController::class, 'destroy']);
 
-    Route::get('/distributors', [DistributorController::class, 'index'])->name('distributors');
-    Route::get('/distributor/details/{distributor:id}', [DistributorController::class, 'details']);
-    Route::delete('/distributor/delete/{distributor:id}', [DistributorController::class, 'destroy']);
+    Route::middleware('server')->group(function () {
+        Route::get('/agents', [AgentsController::class, 'index'])->name('agents');
+        Route::post('/agent/store', [AgentsController::class, 'store'])->name('agent.store');
+        Route::get('/agent/details/{agent:id}', [AgentsController::class, 'details']);
+        Route::delete('/agent/delete/{agent:id}', [AgentsController::class, 'destroy']);
+        Route::get('/distributors', [DistributorController::class, 'index'])->name('distributors');
+        Route::get('/distributor/details/{distributor:id}', [DistributorController::class, 'details']);
+        Route::delete('/distributor/delete/{distributor:id}', [DistributorController::class, 'destroy']);
 
-    Route::get('/distribution', [DistributionController::class, 'index'])->name('distributed');
-    Route::get('/distribution-request', [ShowOrderController::class, 'show'])->name('request.distributor');
+        Route::get('/distribution', [DistributionController::class, 'index'])->name('distributed');
+        Route::get('/distribution-request', [ShowOrderController::class, 'show'])->name('request.distributor');
 
-    Route::get('/agent-container', [ContainerController::class, 'index'])->name('container');
-    Route::post('/agent-container', [ContainerController::class, 'store'])->name('container.store');
-    Route::post('/agent-container/handle/{container:id}', [ContainerController::class, 'handle']);
-    Route::delete('/container/delete/{container:id}', [ContainerController::class, 'destroy']);
+        Route::get('/agent-container', [ContainerController::class, 'index'])->name('container');
+        Route::post('/agent-container', [ContainerController::class, 'store'])->name('container.store');
+        Route::post('/agent-container/handle/{container:id}', [ContainerController::class, 'handle'])->withoutMiddleware('server');
+        Route::delete('/container/delete/{container:id}', [ContainerController::class, 'destroy']);
 
-    Route::get('/post/agent/{post:id}', [AgentPostController::class, 'detail']);
-    Route::get('/post/distributor/{post:id}', [DistributorPostController::class, 'detail']);
+        Route::get('/post/agent/{post:id}', [AgentPostController::class, 'detail']);
+        Route::get('/post/distributor/{post:id}', [DistributorPostController::class, 'detail']);
 
-    Route::get('/warehouse', [WarehouseController::class, 'index']);
-    Route::get('/warehouse/{warehouse:id}', [WarehouseController::class, 'detail']);
-    Route::post('/warehouse', [WarehouseController::class, 'store']);
-    Route::post('/warehouse/product-type', [WarehouseController::class, 'createProductType'])->name('product-type');
+        Route::get('/warehouse', [WarehouseController::class, 'index']);
+        Route::get('/warehouse/{warehouse:id}', [WarehouseController::class, 'detail']);
+        Route::post('/warehouse', [WarehouseController::class, 'store']);
+        Route::post('/warehouse/product-type', [WarehouseController::class, 'createProductType'])->name('product-type');
+    });
 });
