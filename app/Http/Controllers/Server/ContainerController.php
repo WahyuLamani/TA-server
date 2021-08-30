6e130d25d9df74895961206e4cf523c25e1299bf
wahyulamani <wahyu@elektro.polimdo.ac.id>
2021-08-30 19:54:08 +0800

    stack warehouse product on container

--- app/Http/Controllers/Server/ContainerController.php
+++ app/Http/Controllers/Server/ContainerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Server;
 use App\Http\Controllers\Controller;
 use App\Models\Client\{Agent, Container};
 use App\Models\Server\Warehouse;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -33,19 +34,33 @@ class ContainerController extends Controller
         $agent = Agent::find($request->agent_id);
         $warehouse = Warehouse::find($request->warehouse_id);
         if ($warehouse->count_down_amount < $request->product_amount) {
-            session()->flash('error', ucwords('Produk yang di bawa ' . $agent->name . ' Terlalu berlebihan'));
+            session()->flash('error', ucwords('Produk yang akan dibawa ' . $agent->name . ' Melebihi produk gudang'));
             return redirect()->back();
+        }
+        $carbon = Carbon::now();
+        $container = Container::where('agent_id', $request->agent_id)
+            ->where('created_at', 'like', '%' . $carbon->format('y-m-d') . '%')
+            ->whereHas('warehouse', function ($q) use ($warehouse) {
+                $q->whereHas('product_type', function ($q) use ($warehouse) {
+                    $q->where('type', $warehouse->product_type->type)
+                        ->where('unit', $warehouse->product_type->unit);
+                });
+            })->first();
+
+        if (isset($container)) {
+            $container->amount = $request->product_amount + $container->amount;
+            $container->count_down_amount = $request->product_amount + $container->count_down_amount;
+            $container->save();
+        } else {
+            $agent->container()->create([
+                'warehouse_id' => $request->warehouse_id,
+                'amount' => $request->product_amount,
+                'count_down_amount' => $request->product_amount,
+            ]);
         }
 
         $warehouse->count_down_amount = $warehouse->count_down_amount - $request->product_amount;
         $warehouse->save();
-
-        $agent->container()->create([
-            'warehouse_id' => $request->warehouse_id,
-            'amount' => $request->product_amount,
-            'count_down_amount' => $request->product_amount,
-        ]);
-
 
         session()->flash('success', 'Pengambilan produk agent dengan nama : ' . $agent->name . ' ditambahkan !!');
         return redirect()->back();
